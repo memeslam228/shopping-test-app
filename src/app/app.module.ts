a5a0e4fe68f40a60ffc7a15a1f69ab2ed462fac8
memeslam228 <sergeyrybchenko2000@gmail.com>
2019-06-11 19:32:14 +0300

    Sign up without CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,37 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { Routes, RouterModule } from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {environment} from 'src/environments/environment';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SinginComponent } from './auth/singin/singin.component';
+import { SingupComponent } from './auth/singup/singup.component';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from './auth/auth.service';
+
+const appRoutes: Routes = [
+    {path: 'signup', component: SingupComponent}
+];
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        SinginComponent,
+        SingupComponent
     ],
     imports: [
         BrowserModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireDatabaseModule,
         NgbModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        FormsModule,
+        RouterModule.forRoot(appRoutes)
     ],
-    providers: [],
+    providers: [AuthService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
