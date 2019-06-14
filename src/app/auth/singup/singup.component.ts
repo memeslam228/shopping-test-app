c6818f7c91f3b7f37066348b90430e536af21fc8
memeslam228 <sergeyrybchenko2000@gmail.com>
2019-06-14 22:30:04 +0300

    Login without logout func__

---
+++ src/app/auth/singup/singup.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthService} from '../auth.service';
+
+@Component({
+  selector: 'app-singup',
+  templateUrl: './singup.component.html',
+  styleUrls: ['./singup.component.css']
+})
+export class SingupComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  onSignup(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.signupUser(email, password);
+  }
+}
