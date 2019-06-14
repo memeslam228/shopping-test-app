168100a89e1b4c6505b4a08e9e14f3401549fdc2
memeslam228 <sergeyrybchenko2000@gmail.com>
2019-06-14 18:35:48 +0300

    Fixed Sign Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FirebaseTask';
+export class AppComponent implements OnInit {
+    title = 'FirebaseTask';
+
+    ngOnInit() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBtvnrGaqcGDYWgEJuFV5Ivt2Gcg03V_qc',
+            authDomain: 'fir-task-bb3fd.firebaseapp.com',
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {environment} from 'src/environments/environment';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SinginComponent } from './auth/singin/singin.component';
-import { SingupComponent } from './auth/singup/singup.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SinginComponent} from './auth/singin/singin.component';
+import {SingupComponent} from './auth/singup/singup.component';
 import {FormsModule} from '@angular/forms';
 import {AuthService} from './auth/auth.service';
 
@@ -25,7 +25,7 @@ const appRoutes: Routes = [
     ],
     imports: [
         BrowserModule,
-        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
         AngularFireDatabaseModule,
         NgbModule,
         BrowserAnimationsModule,
